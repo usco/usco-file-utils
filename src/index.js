f466e34cef2a98b675bb928890e1b51867c1b7c9
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-09 22:20:10 +0100

    chore(): added all base files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// file utils
+// this one could be replaced with https://www.npmjs.com/package/file-extension
+export function getExtension (fname) {
+  return fname.substr((~-fname.lastIndexOf('.') >>> 0) + 2).toLowerCase()
+}
+
+export function getNameAndExtension (uri) {
+  const _uriElems = uri.split('?')
+  const name = _uriElems.shift().split('/').pop()
+  const ext = getExtension(uri)
+  return {name, ext}
+}
+
+export function isValidFile (file) {
+  return ((typeof file !== 'undefined' && file !== null) && file instanceof File)
+}
